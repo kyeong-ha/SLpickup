159d8711ca7c519b118151e1160d203aab33b3c0
KyeongHa <rudgk030@hanyang.ac.kr>
2023-02-13 13:00:50 +0900

    2023-02-13

--- src/js/header_scroll_event.js
+++ src/js/header_scroll_event.js
@@ -3,11 +3,11 @@ const header = document.querySelector("#header");
 window.addEventListener("scroll", (event) => {
     const scrollY = this.scrollY;
 
-    if(scrollY > 56){
-        header.style.backgroundColor = '#FFF';
+    if(scrollY > 0){
+        // header.style.backgroundColor = 'rgba(255, 255, 255, 0.592)';
         header.style.boxShadow = '0px 2px 8px -4px rgb(152, 152, 152)';
     }else{
-        header.style.backgroundColor = 'transparent';
+        // header.style.backgroundColor = 'transparent';
         header.style.boxShadow = '0px 0px 0px 0px';
     }
 });
