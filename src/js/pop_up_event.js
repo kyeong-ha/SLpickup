ee69e9d9efa2737ee18ba3ce2367ff40cd06c958
KyeongHa <rudgk030@hanyang.ac.kr>
2023-02-27 17:35:42 +0900

    2023-02-27

--- src/js/pop_up_event.js
+++ src/js/pop_up_event.js
@@ -1,17 +1,33 @@
 $(function(){
     $("#privacy_policy_btn").click(function(){        
         $("#privacy_popup").css('display','flex').hide().fadeIn();
+        document.body.style.overflow = 'hidden';
+        document.body.style.touchAction = 'none';
     });
     $("#terms_of_service_btn").click(function(){        
         $("#terms_popup").css('display','flex').hide().fadeIn();
+        document.body.style.overflow = 'hidden';
+        document.body.style.touchAction = 'none';
     });
 
     $("#privacy_confirm").click(function(){
         $("#privacy_popup").fadeOut();
+        document.body.style.overflow = '';
+        document.body.style.touchAction = '';
     });
     $("#terms_confirm").click(function(){
         $("#terms_popup").fadeOut();
+        document.body.style.overflow = '';
+        document.body.style.touchAction = '';
     });
+});
+
+
+$(document).mouseup(function (e){
+	let LayerPopup = $(".popup-wrap");
+	if(LayerPopup.has(e.target).length === 0){
+		LayerPopup.fadeOut();
+	}
 });
 
 document.querySelectorAll('.pop-button').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
